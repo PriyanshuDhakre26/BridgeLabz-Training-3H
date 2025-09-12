f7abd5de74d71293854eb5c3c61f4dafe5fd4836
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2025-09-12 21:17:42 +0530

    Adding program where Course is the base class, OnlineCourse is a subclass, and PaidOnlineCourse extends OnlineCourse

---
+++ Oops/Inheritance_problems/CourseHierarchyDemo.java
@@ -0,0 +1,6 @@
+public class CourseHierarchyDemo {
+    public static void main(String[] args) {
+        PaidOnlineCourse poc = new PaidOnlineCourse("Java Programming", 8, "Udemy", true, 5000, 20);
+        poc.displayDetails();
+    }
+}
