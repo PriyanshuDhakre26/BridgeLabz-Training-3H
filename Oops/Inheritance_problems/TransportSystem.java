a27fca8e08d71dd3257d73c7f24f4a36a706d684
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2025-09-12 18:47:48 +0530

    Adding program where Vehicle is the superclass, and Car, Truck, and Motorcycle are subclasses with unique attributes

---
+++ Oops/Inheritance_problems/TransportSystem.java
@@ -0,0 +1,12 @@
+public class TransportSystem {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 15);
+        vehicles[2] = new Motorcycle(150, "Petrol", "Sports");
+
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+        }
+    }
+}
---
+++ Oops/Inheritance_problems/Vehicle.java
@@ -0,0 +1,55 @@
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    public void displayInfo() {
+        System.out.println("Vehicle - Max Speed: " + maxSpeed + " km/h, Fuel Type: " + fuelType);
+    }
+}
+
+class Car extends Vehicle {
+    int seatCapacity;
+
+    Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Car - Max Speed: " + maxSpeed + " km/h, Fuel Type: " + fuelType + ", Seats: " + seatCapacity);
+    }
+}
+
+class Truck extends Vehicle {
+    int loadCapacity;
+
+    Truck(int maxSpeed, String fuelType, int loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Truck - Max Speed: " + maxSpeed + " km/h, Fuel Type: " + fuelType + ", Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    String type;
+
+    Motorcycle(int maxSpeed, String fuelType, String type) {
+        super(maxSpeed, fuelType);
+        this.type = type;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Motorcycle - Max Speed: " + maxSpeed + " km/h, Fuel Type: " + fuelType + ", Type: " + type);
+    }
+}
