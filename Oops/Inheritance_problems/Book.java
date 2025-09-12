768d6176f6edfcd91ee32c8ab41e8923d21b7b84
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2025-09-12 18:52:15 +0530

    Adding program of Book system where Book is the superclass, and Author is a subclass

---
+++ Oops/Inheritance_problems/Book.java
@@ -0,0 +1,30 @@
+class Book {
+    String title;
+    int publicationYear;
+
+    Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public void displayInfo() {
+        System.out.println("Book - Title: " + title + ", Year: " + publicationYear);
+    }
+}
+
+class Author extends Book {
+    String name;
+    String bio;
+
+    Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Book - Title: " + title + ", Year: " + publicationYear +
+                ", Author: " + name + ", Bio: " + bio);
+    }
+}
---
+++ Oops/Inheritance_problems/LibraryManagement.java
@@ -0,0 +1,6 @@
+public class LibraryManagement {
+    public static void main(String[] args) {
+        Author a = new Author("The Great Gatsby", 1925, "F. Scott Fitzgerald", "American novelist and short story writer");
+        a.displayInfo();
+    }
+}
