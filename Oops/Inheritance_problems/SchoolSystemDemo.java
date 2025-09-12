2977b90bfe89f5a40c80cf07aa9494d136e2ee81
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2025-09-12 21:20:39 +0530

    Adding program  where Person is the superclass, and Teacher, Student, and Staff are subclasses

---
+++ Oops/Inheritance_problems/SchoolSystemDemo.java
@@ -0,0 +1,73 @@
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+class Teacher extends Person {
+    String subject;
+
+    public Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    public void displayRole() {
+        displayDetails();
+        System.out.println("Role: Teacher");
+        System.out.println("Subject: " + subject);
+    }
+}
+
+class Student extends Person {
+    String grade;
+
+    public Student(String name, int age, String grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+
+    public void displayRole() {
+        displayDetails();
+        System.out.println("Role: Student");
+        System.out.println("Grade: " + grade);
+    }
+}
+
+class Staff extends Person {
+    String department;
+
+    public Staff(String name, int age, String department) {
+        super(name, age);
+        this.department = department;
+    }
+
+    public void displayRole() {
+        displayDetails();
+        System.out.println("Role: Staff");
+        System.out.println("Department: " + department);
+    }
+}
+
+public class SchoolSystemDemo {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher("Mr. Sharma", 40, "Mathematics");
+        Student student = new Student("Priya", 16, "10th Grade");
+        Staff staff = new Staff("Ramesh", 35, "Administration");
+
+        teacher.displayRole();
+        System.out.println();
+        student.displayRole();
+        System.out.println();
+        staff.displayRole();
+    }
+}
