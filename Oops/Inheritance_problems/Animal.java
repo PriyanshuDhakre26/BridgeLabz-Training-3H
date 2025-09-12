5de8021616b7d957db24aabf65e5c1f6897601b1
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2025-09-12 18:40:00 +0530

    Adding program where Animal is the superclass, and Dog, Cat, and Bird are subclasses. Each subclass has a unique behavior

---
+++ Oops/Inheritance_problems/Animal.java
@@ -0,0 +1,46 @@
+class Animal {
+    String name;
+    int age;
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Woof Woof!");
+    }
+}
+
+class Cat extends Animal {
+    Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Meow Meow!");
+    }
+}
+
+class Bird extends Animal {
+    Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Tweet Tweet!");
+    }
+}
